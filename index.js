585bce03acc1bd01ea69e385f7b3269d17723c10
Klif <sang.wicklife@student.moringaschool.com>
2023-12-18 08:57:59 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let messages = [];
+    for (const theName of names){
+        messages.push(`Thank you, ${theName}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+
+function countDown(number) {
+    while (number>=0) {
+        console.log(number);
+        number--;
+    }
+}
+
+// this works too
+/* function countDown(number) {
+     for (let i = number; i >= 0; i--) {
+         if(number<0) return;
+         console.log(i);
+    }
+ }
+*/
